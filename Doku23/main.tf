797d3ee1c6b189028478a0188d66635b5c7662a3
fmcmincs <christopher.schmidt@fairmar.com>
2023-01-24 09:31:28 +0100

    Doku23 + main.tf cleanup

--- Doku23/main.tf
+++ Doku23/main.tf
@@ -10,16 +10,24 @@ terraform {
 }
 
 provider "aws" {
+  # Verfügbare Regionen: us-east-1 (N.Virgina) und eu-west-1 (Irland)
   region = "eu-west-1"
+  
+  # Eigenes CLI SSO Profil hier eingeben
   profile = "fmcmincs"
+
+  #bitte diese zwei Zeilen löschen...
   #shared_credentials_files = ["~/.aws/config"]
 }
 
-resource "aws_instance" "app_server_christopher1234323422" {
+resource "aws_instance" "app_server_fmctest" {
   ami           = "ami-0fe0b2cf0e1f25c8a"
   instance_type = "t2.micro"
 
   tags = {
+    # Eigenen Namen hier eingeben
     Name = "fairmarchristopherschmidt"
+
+    Umgebung = "sandbox"
   }
 }
